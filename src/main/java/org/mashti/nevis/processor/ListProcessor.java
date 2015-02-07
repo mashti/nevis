7dc8518eb64b08ee210c354092db0d8ef8f22e1c
masih <masiih@gmail.com>
2015-02-07 17:42:38 +0000

    working on list processor

--- src/main/java/org/mashti/nevis/processor/ListProcessor.java
+++ src/main/java/org/mashti/nevis/processor/ListProcessor.java
@@ -33,26 +33,28 @@ public class ListProcessor extends Processor {
 
     public ListProcessor() {
 
-        super(Pattern.compile("^" +
-                "(\\A|\\n{2,})" +
-                "(" +
-                "(" +
-                "[ ]{0," + 3 + "}" +
-                "((?:[-+*]|\\d+[.]))" + // $3 is first list item marker
-                "[ ]+" +
-                ")" +
-                "(?s:.+?)" +
-                "(" +
-                "\\z" + // End of input is OK
-                "|" +
-                "\\n{2,}" +
-                "(?=\\S)" + // If not end of input, then a new para
-                "(?![ ]*" +
-                "(?:[-+*]|\\d+[.])" +
-                "[ ]+" +
-                ")" + // negative look ahead for another list marker
-                ")" +
-                ")"));
+//        super(Pattern.compile("^" +
+//                "(\\A|\\n{2,})" +
+//                "(" +
+//                "(" +
+//                "[ ]{0," + 3 + "}" +
+//                "((?:[-+*]|\\d+[.]))" + // $3 is first list item marker
+//                "[ ]+" +
+//                ")" +
+//                "(?s:.+?)" +
+//                "(" +
+//                "\\z" + // End of input is OK
+//                "|" +
+//                "\\n{2,}" +
+//                "(?=\\S)" + // If not end of input, then a new para
+//                "(?![ ]*" +
+//                "(?:[-+*]|\\d+[.])" +
+//                "[ ]+" +
+//                ")" + // negative look ahead for another list marker
+//                ")" +
+//                ")"));
+
+        super(Pattern.compile("^( *)((?:[*+-]|\\d+\\.)) [\\s\\S]+?(?:\\n+(?=\\1?(?:[-*_] *){3,}(?:\\n+|$))|\\n+(?= *\\[([^\\]]+)\\]: *<?([^\\s>]+)>?(?: +[\"(]([^\\n]+)[\")])? *(?:\\n+|$))|\\n{2,}(?! )(?!\\1(?:[*+-]|\\d+\\.) )\\n*|\\s*$)"));
 
 
     }
@@ -60,10 +62,9 @@ public class ListProcessor extends Processor {
     @Override
     public Optional<Node> process(final Matcher matcher, Parser parser) {
 
-        final String first_marker = matcher.group(3);
-        String items = matcher.group(2);
-        items = Pattern.compile("\\n{2,}").matcher(items).replaceAll("\n\n\n");
-        final List list = new List(!StringUtils.containsAny(first_marker, new char[]{'-', '+', '*'}));
+        final boolean ordered = matcher.group(2).length() > 1;
+        final String items = matcher.group();
+        final List list = new List(ordered);
 
         processItems(list, items, parser);
         return Optional.of(list);
@@ -71,26 +72,30 @@ public class ListProcessor extends Processor {
 
     private void processItems(List parent, String items, Parser parser) {
 
-        items = Pattern.compile("\\n{2,}\\z").matcher(items).replaceAll("\n");
-
-        Pattern p = Pattern.compile("(\\n*)?" +
-                "^([ \\t]*)([-+*]|\\d+[.])[ ]+" +
-                "((?s:.+?)(\\n{1,2}|\\z))" +
-                "(?=\\n*(\\z|\\2([-+\\*]|\\d+[.])[ \\t]+))", Pattern.MULTILINE);
-
+        final Pattern p = Pattern.compile("^( *)((?:[*+-]|\\d+\\.)) [^\\n]*(?:\\n(?!\\1(?:[*+-]|\\d+\\.) )[^\\n]*)*", Pattern.MULTILINE);
         final Matcher matcher = p.matcher(items);
 
+        final Pattern remove_bullets = Pattern.compile("^ *([*+-]|\\d+\\.) +");
         while (matcher.find()) {
-            final ListItem list_item = new ListItem();
-            String leadingLine = matcher.group(1);
-            String split_item = matcher.group(4);
-            split_item = Pattern.compile("^[ ]{4,}", Pattern.MULTILINE).matcher(split_item).replaceAll("");
 
-            if (!(leadingLine == null || leadingLine.isEmpty()) || split_item.contains("\n\n")) {
-                parser.parseBlock(list_item, split_item);
-            } else {
-                parser.parseInline(list_item, split_item.trim());
-            }
+            String item = matcher.group();
+            item = remove_bullets.matcher(item).replaceAll("");
+            System.out.println(item);
+
+            final ListItem list_item = new ListItem();
+            parser.parseBlock(list_item, item);
+            
+            
+//            String leadingLine = matcher.group(1);
+//            String split_item = matcher.group(4);
+//            split_item = Pattern.compile("^[ ]{4,}", Pattern.MULTILINE).matcher(split_item).replaceAll("");
+//
+//            if (!(leadingLine == null || leadingLine.isEmpty()) || split_item.contains("\n\n")) {
+//                parser.parseBlock(list_item, split_item);
+//            } else {
+//                parser.parseInline(list_item, split_item.trim());
+//            }
+            
             parent.addChild(list_item);
         }
     }
